13032e8301f90a502ba4d83d6b6c943dc4a25014
lucasbass82 <lucasbass82@gmail.com>
2026-04-24 12:15:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2071,7 +2071,7 @@ const styles: Record<string, React.CSSProperties> = {
     textAlign: "left",
   },
   searchBar: {
-    marginTop: 10,
+    marginTop: 2,
     width: "100%",
     display: "flex",
     alignItems: "center",
